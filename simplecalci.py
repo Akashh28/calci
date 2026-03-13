033b9f3f8164212be4b5ea2a813c8e597a0a58a8
k akash <kakash890@gmail.com>
2026-03-13 11:50:39 +0530

    changed code

--- simplecalci.py
+++ simplecalci.py
@@ -11,7 +11,7 @@ choice = input("Enter choice (1/2/3/4): ")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-if choice == '201':
+if choice ==   '1'
     print("Result =", num1 + num2)
 
 elif choice == '2':
